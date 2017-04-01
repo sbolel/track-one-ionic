565b51eb03e23e3c8afb60970806164f28fe80f4
Sinan Bolel <sinanbolel@gmail.com>
2017-04-01 07:16:09 -0500

    chore: initial scaffold

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core'
 import { Platform } from 'ionic-angular'
 import { SplashScreen } from '@ionic-native/splash-screen'
 import { StatusBar } from '@ionic-native/status-bar'
-import { TabsPage } from '../pages/tabs/tabs'
+import Home from '../pages/home/home'
 
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = TabsPage
+export default class App {
+  rootPage:any = Home
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,22 @@ import { NgModule, ErrorHandler } from '@angular/core'
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular'
 import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
-import { MyApp } from './app.component'
-import { AboutPage } from '../pages/about/about'
-import { ContactPage } from '../pages/contact/contact'
-import { HomePage } from '../pages/home/home'
-import { TabsPage } from '../pages/tabs/tabs'
+import App from './app.component'
+import Home from '../pages/home/home'
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(App)
   ],
-  bootstrap: [
-    IonicApp
+  declarations: [
+    App,
+    Home
   ],
   entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    App,
+    Home
   ],
+  bootstrap: [IonicApp],
   providers: [
     StatusBar,
     SplashScreen,
